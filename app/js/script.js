e86241ef56403dc67c38fad044319df79f8dee41
stas257742 <stas257742@mail.ru>
2020-03-31 15:18:36 +0300

    Адаптирую хули...

--- app/js/script.js
+++ app/js/script.js
@@ -63,6 +63,60 @@ function showSlides(n) {
 }
 
 
+/*Слайдер для команды*/
+
+$(window).resize(function() {
+    if ($(window).width() < 1024) {
+        /* Индекс слайда по умолчанию */
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        /* Функция увеличивает индекс на 1, показывает следующй слайд*/
+        function plusSlide() {
+            showSlides(slideIndex += 1);
+        }
+
+        /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+        function minusSlide() {
+            showSlides(slideIndex -= 1);
+        }
+
+        /* Устанавливает текущий слайд */
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("item1");
+            var dots = document.getElementsByClassName("slider-dots_item1");
+            if (n > slides.length) {
+                slideIndex = 1
+            }
+            if (n < 1) {
+                slideIndex = slides.length
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    }
+});
+
+$(window).resize(function() { /* что бы когда расширяешь экран после 1024 элементы возвращались в исходное положение*/
+    if ($(window).width() > 1024) {
+        showSlides = 0;
+        $('.item1').css('display', 'block');
+    }
+});
+
+
 
 
 /* Скрытие или раскрытие меню */
